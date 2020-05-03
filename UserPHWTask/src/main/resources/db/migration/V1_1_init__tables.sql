75d57eec14c8518ff82a76e2ce73eb8700112a5a
jamesohmcts <james.odukoya@hmcts.net>
2020-05-03 15:54:55 +0100

    Userprofile sql

--- UserPHWTask/src/main/resources/db/migration/V1_1_init__tables.sql
+++ UserPHWTask/src/main/resources/db/migration/V1_1_init__tables.sql
@@ -1,11 +1,11 @@
-CREATE
+
 
 create table user_profile(
     user_identifier bigint,
     email_address varchar(255),
     first_name varchar(255),
     last_name varchar(255),
-    created timestamp,
+    created_date timestamp,
     last_updated timestamp
 );
 
